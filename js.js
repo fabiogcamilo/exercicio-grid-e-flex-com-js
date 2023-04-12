6e46017fd82eaabe898f9f1237d583e812be7d6b
fabiogcamilo <fabiogcamilo@gmail.com>
2023-04-12 20:38:57 -0300

    100%

--- js.js
+++ js.js
@@ -1,57 +1,32 @@
-console.log("Teste")
-
-
 function executarSoma() {
-    // Capturar os elementos para usar depois
-    const toast = document.getElementById("toast")
-    const elResultadoSoma = document.getElementById("resultadoSoma")
-    const saidaExterna = document.getElementById("saidaExterna")
-    saidaExterna.innerHTML = "<br>"
-
-    // Reset
-    toast.style.display = "none"
-    elResultadoSoma.innerHTML = 0
-
-    // Entradas
-    // Dos elementos inputs do HTML
-    const primeiroInput = document.getElementById("primeiroInput")
-    const segundoInput = document.getElementById("segundoInput")
-
-    const primeiroNumero = primeiroInput.value
-    const segundoNumero = segundoInput.value
-
-    // Validação
-    if (primeiroNumero == "" || segundoNumero == "") {
-        toast.style.display = "block"
-
-        return
+    // Pegar valores dos inputs
+    const primeiroValor = parseFloat(document.getElementById("primeiroInput").value);
+    const segundoValor = parseFloat(document.getElementById("segundoInput").value);
+  
+    // Verifica se os valores são válidos
+    if (isNaN(primeiroValor) || isNaN(segundoValor)) {
+      alert("Por favor, preencha os dois valores para realizar a soma.");
+      return;
     }
+  
+    // Realiza a soma dos valores e exibe o resultado
+    const resultado = primeiroValor + segundoValor;
+    document.getElementById("resultadoSoma").textContent = resultado;
+  }
 
-    // Processamento
-    const resultado = Number(primeiroNumero) + Number(segundoNumero)
-
-    // Saída
-    // Lá na tela, onde tem o card exercicio
-    elResultadoSoma.innerHTML = resultado
-}
-
-
-async function executarPokemon() {
-    // Entradas
-    const elIdPokemon = document.getElementById("idPokemon")
-    const idPokemonEscolhido = elIdPokemon.value
-
-    // Processamento
-    // Ir na API buscar o pokemon
-    const url = `https://pokeapi.co/api/v2/pokemon/${idPokemonEscolhido}`
-    const retornoFetch = await fetch(url)
-    const pokemon = await retornoFetch.json()
-
-    // Saida
-    // console.log(pokemon)
-    const elNomePokemon = document.getElementById("nomePokemon")
-    const elImagemPokemon = document.getElementById("imagemPokemon")
-
-    elNomePokemon.innerHTML = pokemon.name
-    elImagemPokemon.src = pokemon.sprites.front_default
-}
+  function executarPokemon() {
+    const idPokemon = document.getElementById("idPokemon").value;
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+      .then(response => response.json())
+      .then(data => {
+        const nomePokemon = data.name;
+        const imagemPokemon = data.sprites.front_default;
+        
+        document.getElementById("nomePokemon").textContent = nomePokemon;
+        document.getElementById("imagemPokemon").src = imagemPokemon;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
